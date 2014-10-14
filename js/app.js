b1faea500871810f95e39d276731cafc4e2d2561
Adam Burdett <burdettadam@gmail.com>
2014-10-14 14:39:15 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-empty
+(function($)
+{
+     var router=new $.mobile.Router( [
+       {"#fue": {handler: "foo",
+          events: "s", // do when we show the page
+          argsre: true
+        } }]
+        ,
+        { foo: function(type, match, ui, page) {
+
+        }}
