ef921a13071f2566bf9d023f66c8fddb770bc485
Adam Burdett <burdettadam@gmail.com>
2014-10-20 12:46:49 -0600

    closer

--- _site/js/app.js
+++ _site/js/app.js
@@ -1,1 +1,37 @@
-empty
+(function($)
+{
+    var router=new $.mobile.Router( [
+       	{"#login": {handler: "login",
+          events: "s", // do when we show the page
+          argsre: true
+        } },
+        {"#Create-Account": {handler: "CreateAccount",
+          events: "s", // do when we show the page
+          argsre: true
+        } }
+	],
+	{ 
+        login: function(type, match, ui, page) {
+        	console.log("login Handler");
+        },
+        CreateAccount: function(type, match, ui, page) {
+            console.log("Create-Account Handler");
+        }
+    });
+
+
+    function onMobileInit() {
+    console.log("mobile init");
+    $.mobile.autoInitialize = false;
+    }
+
+    function onPageLoad() {// Document.Ready
+    console.log("document ready");
+    //CloudOS.retrieveSession();
+
+    /////////////////////////////////////////////////////////////////////
+    // this is the actual code that runs and sets everything off
+    // pull the session out of the cookie.
+    $(document).bind("mobileinit", onMobileInit);
+    $(document).ready(onPageLoad);
+})(jQuery);
