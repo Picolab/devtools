7f8cb5d26e8a8224bbabba559c5e69e527341768
Ashlee <rupp.ashlee@gmail.com>
2014-12-03 17:28:47 -0700

    adding more jquery

---
+++ js/jquery.mobile-1.4.0-extentions.js
@@ -0,0 +1,39 @@
+﻿
+$.extend($.mobile, {
+	changePageFromBase: function(base, to, options)
+	{
+		var el = $(to),
+			toid = to.substr(to.lastIndexOf('-') + 1),
+			toidC = toid.substr(0, 1).toUpperCase() + toid.substr(1),
+			str,
+			basel,
+			newel;
+
+		console.log('toid=', toid);
+		console.log('toidC=', toidC);
+
+		if (el.length) {
+			$.mobile.changePage(to, options);
+			return;
+		}
+
+		basel = $(base).clone()
+		console.log('base=', base);
+
+		console.info('basel.html()=', basel.html());
+		str = basel.html() || "";
+
+		str = str.replace('create_edit', toid)
+				 .replace('Create_Edit', toidC);
+		//console.info(str);
+
+		str = '<div data-role="page" id="page-manager-location-' + toid + '">'
+			+ str
+			+ "</div>";
+		console.info(str);
+
+		$("body").append(str);
+
+		$.mobile.changePage(to, options);
+	}
+});
