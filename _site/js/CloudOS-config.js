7678a2fd7196088cbf0086481677f44ad0c76631
Adam Burdett <burdettadam@gmail.com>
2014-12-29 15:48:24 -0700

    work towards a sidebar menu.

--- _site/js/CloudOS-config.js
+++
@@ -1,6 +0,0 @@
-
-CloudOS.appKey = "6E461F82-7A80-11E4-9E1F-8CA1E70EDEE7";
-CloudOS.anonECI = "85255500-0b65-0130-243c-00163ebcdddd";
-CloudOS.callbackURL = "http://kre.github.io/devtools/code.html";
-CloudOS.host = "cs.kobj.net";
-CloudOS.login_server = "kibdev.kobj.net";
