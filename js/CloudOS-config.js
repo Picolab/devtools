914f039356fd9c67d090b7cf9c5c2c02fee68933
Phil Windley <phil@windley.org>
2015-02-23 16:27:40 -0700

    Update config to use kibdev

--- js/CloudOS-config.js
+++ js/CloudOS-config.js
@@ -1,6 +1,5 @@
-
 CloudOS.appKey = "6E461F82-7A80-11E4-9E1F-8CA1E70EDEE7";
 CloudOS.anonECI = "85255500-0b65-0130-243c-00163ebcdddd";
 CloudOS.callbackURL = "http://kre.github.io/devtools/code.html";
-CloudOS.host = "cs.kobj.net";
-CloudOS.login_server = "kibdev.kobj.net";
+CloudOS.host = "kibdev.kobj.net"; // change to cs.kobj.net when in production
+CloudOS.login_server = "kibdev.kobj.net"; // change to accounts.kobj.net when in production
