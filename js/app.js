fe06e588bbd1104442f677cfca8db74fa92a8b40
Adam Burdett <burdettadam@gmail.com>
2015-03-03 14:43:32 -0700

    correct function call

--- js/app.js
+++ js/app.js
@@ -123,7 +123,7 @@
                 console.log("installed ", rid, directives);
                 $.mobile.changePage("#page-installed-rulesets", {
                   transition: 'slide' //all of this found in install_rulesets */
-                Devtools.updateURL(rid, url, function(directives){
+                Devtools.updateUrl(rid, url, function(directives){
                   console.log("updating the function is running", rid, directives);
                   $.mobile.changePage("#listing", {
                     transition: 'slide'
