c22540f66c82b960e4b7b1f9f872cf0f08546b3e
Adam Burdett <burdettadam@gmail.com>
2015-03-10 13:31:26 -0600

    logging

--- js/app.js
+++ js/app.js
@@ -293,7 +293,7 @@
              var channels = channel_list["channels"];
              console.log("channels[channels]", channels);
              $.each(channels, function(channel) {
-              console.log("inside, channel passed in: ")
+              console.log("inside, channel passed in: ", channel)
                 $("#installed-channels" ).append(
                  snippets.installed_channels_template(
                   {"channel_name": channel["name"],
