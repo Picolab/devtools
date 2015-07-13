d222f213cea7d6fd1dba7d1c1871ba1cad2bb43d
Adam Burdett <burdettadam@gmail.com>
2015-07-13 15:12:43 -0600

    nano manager.js

---
+++ js/nano_manager-config.js
@@ -0,0 +1,5 @@
+nano_manager.appKey = "84920CCC-1082-11E5-BAFA-BC3F2D0A6675";
+nano_manager.anonECI = "85255500-0b65-0130-243c-00163ebcdddd";
+nano_manager.callbackURL = "http://0.0.0.0:4000/code.html";
+nano_manager.host = "kibdev.kobj.net"; // change to cs.kobj.net when in production
+nano_manager.login_server = "kibdev.kobj.net"; // change to accounts.kobj.net when in production
