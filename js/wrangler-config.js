c6f3c0027e5587720ab70da2b127bdba1f3e2b91
Adam Burdett <burdettadam@gmail.com>
2016-01-20 15:57:20 -0700

    jsdocs
    some code cleaning but mostly the start of jsdoc.

--- js/wrangler-config.js
+++ js/wrangler-config.js
@@ -3,7 +3,9 @@ wrangler.anonECI = "85255500-0b65-0130-243c-00163ebcdddd";
 wrangler.callbackURL = "http://kre.github.io/devtools/code.html";
 wrangler.host = "kibdev.kobj.net"; // change to cs.kobj.net when in production
 wrangler.login_server = "kibdev.kobj.net"; // change to accounts.kobj.net when in production
- // oAuth key and eci for local jekyll develpment. 
+wrangler.eventPath = 'sky/event';
+wrangler.functionPath ='sky/cloud';
+ // oAuth key and eci for local jekyll development. 
 /*wrangler.appKey = "84920CCC-1082-11E5-BAFA-BC3F2D0A6675";
 wrangler.anonECI = "85255500-0b65-0130-243c-00163ebcdddd";
 wrangler.callbackURL = "http://0.0.0.0:4000/code.html";
