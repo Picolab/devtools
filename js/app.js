985d48855b474c1c4d0799ea2541b29fc25c844a
Adam Burdett <burdettadam@gmail.com>
2016-02-09 12:45:03 -0700

    no sort

--- js/app.js
+++ js/app.js
@@ -300,7 +300,8 @@
 				function populate_registered_rulesets(){	
 					Devtools.getRulesets(function(rids_json){ //the callback/function is where we need to have all of our code
 						$("#manage-list" ).empty();
-						var sortedRids = rids_json['description'].sort(sortBy("rid"));
+						//var sortedRids = rids_json['description'].sort(sortBy("rid"));
+						var sortedRids = rids_json['description']);
 
 						dynamicRegRulesets="";
 						$.each(sortedRids, function (id, rids) {
